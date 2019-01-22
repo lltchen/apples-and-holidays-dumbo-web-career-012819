ed9c3c9af62b27fdde7eb498323793f8163611d4
lltchen <lltchen1@gmail.com>
2019-01-22 21:53:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
                          }
                        }
  end
- 
+
  def second_supply_for_fourth_of_july(holiday_supplies)
    holiday_supplies[:summer][:fourth_of_july][1]
  end
@@ -26,7 +26,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 def add_supply_to_winter_holidays(holiday_hash, supply)
    holiday_hash[:winter][:christmas] << supply
    holiday_hash[:winter][:new_years] << supply
-   
+
 end
 
 
