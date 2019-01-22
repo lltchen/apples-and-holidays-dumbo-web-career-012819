d5773b7ea495ee2f31aab927e644c59bc67eb1e2
lltchen <lltchen1@gmail.com>
2019-01-22 23:14:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].values
+  apple = holiday_hash[:winter].values
+  apple.to_s
 end
 
 def all_supplies_in_holidays(holiday_hash)
