6bd950bd17d5967a3345d896a4c9e352533b047c
lltchen <lltchen1@gmail.com>
2019-01-23 00:14:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
     values.each do |atrr, value|
       apple = atrr.to_s.capitalize
       cat = value.to_s
-      
+
       puts apple + ":" + cat
     end
   end
